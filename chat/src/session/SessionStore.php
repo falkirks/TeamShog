7114a37fe5d3288c49335e97bb97c923449b23ad
Falkirks <bpnoreply@gmail.com>
2014-11-15 08:21:28 -0800

    Added empty methods to SessionStore and created placeholder User class

--- chat/src/session/SessionStore.php
+++ chat/src/session/SessionStore.php
@@ -1,7 +1,17 @@
 <?php
+namespace shogchat\session;
 /*
  * This class will manage session cookied and allow the opening a session via
  * a user object and getting a user by there session ID.
  */
 class SessionStore{
+    public function hasSession(){
+
+    }
+    public function getCurrentSession(){
+
+    }
+    public function createSession(User $user){
+
+    }
 }
---
+++ chat/src/session/User.php
@@ -0,0 +1,9 @@
+<?php
+namespace shogchat\session;
+/*
+ * This class might be in the wrong namespace
+ * Don't worry about moving it, users exist outside of sessions.
+ */
+class User{
+
+}
