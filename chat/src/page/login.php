7ac991944861c2e32b2e6d7e300a6582d449e3f6
Falkirks <bpnoreply@gmail.com>
2014-11-15 09:24:43 -0800

    Added password verification to login

--- chat/src/page/login.php
+++ chat/src/page/login.php
@@ -1,10 +1,28 @@
 <?php
 namespace shogchat\page;
 
+use shogchat\database\Users;
+
 class login extends Page{
     public function showPage(){
         if(isset($_POST["login-username"]) && isset($_POST["login-password"])){
-            //TODO do checking and give session cookie
+            if(Users::checkLogin($_POST["login-username"], $_POST["login-password"])){
+                //TODO give them a session and redirect
+                echo $this->getTemplateEngine()->render($this->getTemplateSnip("page"), [
+                    "title" => "Login",
+                    "content" => $this->getTemplateEngine()->render($this->getTemplate(), [
+                        "error" => "Logged in."
+                    ])
+                ]);
+            }
+            else{
+                echo $this->getTemplateEngine()->render($this->getTemplateSnip("page"), [
+                    "title" => "Login",
+                    "content" => $this->getTemplateEngine()->render($this->getTemplate(), [
+                        "error" => "Your username or password appear incorrect."
+                    ])
+                ]);
+            }
         }
         else {
             echo $this->getTemplateEngine()->render($this->getTemplateSnip("page"), [
