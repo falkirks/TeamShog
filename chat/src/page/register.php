0afaacfd4f2edf2840c79e500dc0cfeb96aa4189
Falkirks <bpnoreply@gmail.com>
2014-11-14 18:54:23 -0800

    Added registration verification

--- chat/src/page/register.php
+++ chat/src/page/register.php
@@ -1,15 +1,30 @@
 <?php
 namespace shogchat\page;
 
+use Github\Client;
+
 class register extends Page{
     public function showPage(){
         if(isset($_POST["register-data"])){
-            echo $this->getTemplateEngine()->render($this->getTemplateSnip("page"), [
-                "title" => "Register",
-                "content" => $this->getTemplateEngine()->render($this->getTemplate(), [
-                    "error" => "Oh no! Something went awry with your registration."
-                ])
-            ]);
+            try{
+                $client = new Client();
+                $client->authenticate($_POST["register-data"], Client::AUTH_HTTP_TOKEN);
+                $users = $client->api('current_user')->follow()->all();
+                echo $this->getTemplateEngine()->render($this->getTemplateSnip("page"), [
+                    "title" => "Register",
+                    "content" => $this->getTemplateEngine()->render($this->getTemplate(), [
+                        "success" => true
+                    ])
+                ]);
+            }
+            catch(\Exception $e) {
+                echo $this->getTemplateEngine()->render($this->getTemplateSnip("page"), [
+                    "title" => "Register",
+                    "content" => $this->getTemplateEngine()->render($this->getTemplate(), [
+                        "error" => "Oh no! Something went awry with your registration."
+                    ])
+                ]);
+            }
         }
         else {
             echo $this->getTemplateEngine()->render($this->getTemplateSnip("page"), [
