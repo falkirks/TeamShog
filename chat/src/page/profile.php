5a530b154e16bf3a307a071238e0631b14e55ed8
Falkirks <bpnoreply@gmail.com>
2014-11-15 13:52:53 -0800

    Remote session closing (why not?)

--- chat/src/page/profile.php
+++ chat/src/page/profile.php
@@ -8,7 +8,12 @@ use shogchat\session\SessionStore;
 class profile extends Page{
     public function showPage(){
         $user = SessionStore::getCurrentSession();
-        $user["sessions"] = array_values($user["sessions"]);
+        $sessions = [];
+        foreach($user["sessions"] as $id => $data){
+            $data["id"] = $id;
+            $sessions[] = $data;
+        }
+        $user["sessions"] = $sessions;
         if(isset($_POST['new-password'])){
             if(strlen($_POST['new-password']) >= 6){
                 Users::changePassword($user["_id"], $_POST['new-password']);
@@ -16,7 +21,7 @@ class profile extends Page{
                     "title" => "Profile",
                     "content" => $this->getTemplateEngine()->render($this->getTemplate(), [
                         "user" => $user,
-                        "changesuccess" => true
+                        "message" => "Your password has been changed."
                     ])
                 ]);
             }
@@ -25,7 +30,7 @@ class profile extends Page{
                     "title" => "Profile",
                     "content" => $this->getTemplateEngine()->render($this->getTemplate(), [
                         "user" => $user,
-                        "changefailed" => true
+                        "error" => "Failed to change password. Are you sure it's six chars or longer?"
                     ])
                 ]);
             }
@@ -46,7 +51,7 @@ class profile extends Page{
                     "title" => "Profile",
                     "content" => $this->getTemplateEngine()->render($this->getTemplate(), [
                         "user" => $user,
-                        "reposupdated" => true
+                        "message" => "Your repositories have been reloaded."
                     ])
                 ]);
             }
@@ -55,10 +60,23 @@ class profile extends Page{
                     "title" => "Profile",
                     "content" => $this->getTemplateEngine()->render($this->getTemplate(), [
                         "user" => $user,
-                        "repoupdatefailed" => true
+                        "error" => "Failed to update repositories."
                     ])
                 ]);
             }
+        }
+        elseif(isset($_GET["changetoken"])){
+            echo "This feature is still being worked on.";
+        }
+        elseif(isset($_GET["destroysession"])){
+            Users::deleteSession($user['_id'], $_GET["destroysession"]);
+            echo $this->getTemplateEngine()->render($this->getTemplateSnip("page"), [
+                "title" => "Profile",
+                "content" => $this->getTemplateEngine()->render($this->getTemplate(), [
+                    "user" => $user,
+                    "message" => "The session has been closed. The client is now logged out."
+                ])
+            ]);
         }
         else{
             echo $this->getTemplateEngine()->render($this->getTemplateSnip("page"), [
@@ -69,6 +87,7 @@ class profile extends Page{
             ]);
         }
     }
+
     public function hasPermission(){
         return SessionStore::hasSession();
     }
