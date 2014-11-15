5fa9b1dbdcf83a077fd37474707d0348e3d8bf3b
Falkirks <bpnoreply@gmail.com>
2014-11-14 18:35:25 -0800

    Added registration error message

--- chat/src/page/register.php
+++ chat/src/page/register.php
@@ -3,10 +3,20 @@ namespace shogchat\page;
 
 class register extends Page{
     public function showPage(){
-        echo $this->getTemplateEngine()->render($this->getTemplateSnip("page"), [
-            "title" => "Register",
-            "content" => $this->getTemplateEngine()->render($this->getTemplate(), [])
-        ]);
+        if(isset($_POST["register-data"])){
+            echo $this->getTemplateEngine()->render($this->getTemplateSnip("page"), [
+                "title" => "Register",
+                "content" => $this->getTemplateEngine()->render($this->getTemplate(), [
+                    "error" => "Oh no! Something went awry with your registration."
+                ])
+            ]);
+        }
+        else {
+            echo $this->getTemplateEngine()->render($this->getTemplateSnip("page"), [
+                "title" => "Register",
+                "content" => $this->getTemplateEngine()->render($this->getTemplate(), [])
+            ]);
+        }
         //echo $this->getTemplateEngine()->render($this->getTemplate(), []);
     }
     public function hasPermission(){
