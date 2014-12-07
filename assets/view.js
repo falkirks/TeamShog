eff0f4786d6727b24713b8f22cb4d3079bea8ed1
Falkirks <bpnoreply@gmail.com>
2014-12-07 00:07:03 -0800

    Increase minSize

--- assets/view.js
+++ assets/view.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-    WordCloud(document.getElementById('wordCloud'), { list: cloudData, gridSize: 16} );
+    WordCloud(document.getElementById('wordCloud'), { list: cloudData, gridSize: 16, minSize: 10} );
 });
