e5a915f44060ca5fd58ffebdceb30b2a7b86d1fb
Leon Chang <99leonchang@gmail.com>
2014-12-07 09:36:04 +0800

    PHP validation

--- src/page/index.php
+++ src/page/index.php
@@ -3,18 +3,25 @@ namespace water\page;
 
 use water\api\Aylien;
 use water\domains\LegalFinder;
+use water\PageRouter;
 use water\session\SessionStore;
 
 class index extends Page{
     public function showPage($message = false){
         $user = SessionStore::getCurrentSession();
 
-        $params = array('url' => 'https://www.digitalocean.com/legal/terms/');
-        $summary = Aylien::call_api('summary', $params);
-        var_dump($summary);
-
         if(!empty($_POST["name"])) {
-            
+            if(count($_POST)<=2){
+                echo $this->getTemplateEngine()->render($this->getTemplateSnip("page"), [
+                    "title" => "Login",
+                    "content" => $this->getTemplateEngine()->render($this->getTemplate(), [
+                        "message" => "Search term must be at least 3 characters long",
+                        "user" => $user
+                    ])
+                ]);
+            }else{
+                //TODO:Search function in JS
+            }
         }else{
             echo $this->getTemplateEngine()->render($this->getTemplateSnip("page"), [
                 "title" => "Welcome!",
