51f1474a0aba6a781af13b3c0f2d92a13192c225
Falkirks <bpnoreply@gmail.com>
2014-12-07 10:00:37 -0800

    Started work on progress bar generation

--- assets/view.js
+++ assets/view.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
     WordCloud(document.getElementById('wordCloud'), { list: cloudData, gridSize: 16, weightFactor: 10} );
+    var generateRatingBoxes = function(){
+        $('.rating-holder .rating-bars').each(function(index){
+            alert(index);
+        });
+    };
+    generateRatingBoxes();
 });
