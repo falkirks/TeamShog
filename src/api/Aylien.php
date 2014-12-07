2057189ea9d4a72bc3d57fd09649cca9a7da52b5
Leon Chang <99leonchang@gmail.com>
2014-12-07 13:33:29 +0800

    Reverting from json_dump

--- src/api/Aylien.php
+++ src/api/Aylien.php
@@ -19,8 +19,7 @@ class Aylien {
         if($response === false){
             return curl_error($ch);
         }else {
-            //return json_decode($response);
-            return var_dump($response);
+            return json_decode($response);
         }
     }
 } 
