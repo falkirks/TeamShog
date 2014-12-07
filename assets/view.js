51fa77c070c98425372aae557c62f3415b698f85
Falkirks <bpnoreply@gmail.com>
2014-12-07 00:02:20 -0800

    Tweaked word cloud

--- assets/view.js
+++ assets/view.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-    WordCloud(document.getElementById('wordCloud'), { list: cloudData } );
+    WordCloud(document.getElementById('wordCloud'), { list: cloudData, minSize: 20} );
 });
