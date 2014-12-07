265424e5599ad819b760cdc6b764d132d84b6889
Falkirks <bpnoreply@gmail.com>
2014-12-07 10:13:09 -0800

    Added more progres bar rendering

--- assets/view.js
+++ assets/view.js
@@ -2,7 +2,10 @@ $(document).ready(function(){
     WordCloud(document.getElementById('wordCloud'), { list: cloudData, gridSize: 16, weightFactor: 10} );
     var generateRatingBoxes = function(){
         $('.rating-holder .rating-bars').each(function(index){
-            alert(index);
+            var up = votes[index][0].length;
+            var down = votes[index][1].length;
+            var total = up+down;
+            $(this).html('<div class="progress-bar progress-bar-success" style="width:'+ (up/(total))*100 + '%">'+ up + '</div><div class="progress-bar progress-bar-danger" style="width:' + (down/(total))*100 + '%">' + down + '</div>');
         });
     };
     generateRatingBoxes();
