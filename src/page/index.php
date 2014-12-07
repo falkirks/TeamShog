457ecbf4b5d822a30f5d863862f93032c277bc9c
Leon Chang <99leonchang@gmail.com>
2014-12-07 09:15:45 +0800

    Testing

--- src/page/index.php
+++ src/page/index.php
@@ -1,20 +1,30 @@
 <?php
 namespace water\page;
 
+use water\api\Aylien;
 use water\domains\LegalFinder;
 use water\session\SessionStore;
 
 class index extends Page{
     public function showPage($message = false){
         $user = SessionStore::getCurrentSession();
+
+        $params = array('url' => 'https://www.digitalocean.com/legal/terms/');
+        $summary = Aylien::call_api('summary', $params);
+        var_dump($summary);
+
+        if(!empty($_POST["name"])) {
+            
+        }else{
+            echo $this->getTemplateEngine()->render($this->getTemplateSnip("page"), [
+                "title" => "Welcome!",
+                "content" => $this->getTemplateEngine()->render($this->getTemplate(), [
+                    "message" => $message,
+                    "user" => $user
+                ])
+            ]);
+        }
         //var_dump(LegalFinder::getLegalDomain("github.com"));
-        echo $this->getTemplateEngine()->render($this->getTemplateSnip("page"), [
-            "title" => "Welcome!",
-            "content" => $this->getTemplateEngine()->render($this->getTemplate(), [
-                "message" => $message,
-                "user" => $user
-            ])
-        ]);
     }
     public function hasPermission(){
         return true;
