38472c61fbd49c4e2168f081f253f3ed50d5c590
Leon Chang <99leonchang@gmail.com>
2014-12-07 20:04:25 +0800

    Add legal page
    TODO: Populate legal page

---
+++ src/page/legal.php
@@ -0,0 +1,22 @@
+<?php
+namespace water\page;
+
+use water\session\SessionStore;
+
+class legal extends Page{
+    public function showPage($message = false){
+        $user = SessionStore::getCurrentSession();
+
+        echo $this->getTemplateEngine()->render($this->getTemplateSnip("page"), [
+            "title" => "Legal",
+            "content" => $this->getTemplateEngine()->render($this->getTemplate(), [
+                "message" => $message,
+                "user" => $user
+            ])
+        ]);
+    }
+
+    public function hasPermission(){
+        return true;
+    }
+}
