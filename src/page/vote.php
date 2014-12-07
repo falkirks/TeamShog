f91e31898ecf53a156955651379523ba98e51626
Falkirks <bpnoreply@gmail.com>
2014-12-07 09:50:31 -0800

    Added first version of /vote endpoint

---
+++ src/page/vote.php
@@ -0,0 +1,51 @@
+<?php
+namespace water\page;
+use water\database\Domains;
+use water\domains\DomainCache;
+use water\session\SessionStore;
+
+class vote extends Page{
+    public function showPage(){
+        $user = SessionStore::getCurrentSession();
+        if(!empty($_GET["dir"]) && !empty($_GET["domain"]) && !empty($_GET["doc"]) && !empty($_GET["sentence"])){
+            $doc = DomainCache::getDocument($_GET["domain"], $_GET["doc"]);
+            if($doc !== false && isset($doc["summary"][$_GET["sentence"]])){
+                if($_GET["dir"] === "up"){
+                    if(in_array($user['_id'], $doc["summary"][$_GET["sentence"]]["up"])){
+                        echo "false";
+                    }
+                    else {
+                        if (($pos = array_search($user['_id'], $doc["summary"][$_GET["sentence"]]["down"])) !== false) {
+                            unset($doc["summary"][$_GET["sentence"]]["down"][$pos]);
+                        }
+                        $doc["summary"][$_GET["sentence"]]["up"][] = $user["_id"];
+                        Domains::setDocument($_GET["domain"], $_GET["doc"], $doc);
+                        echo json_encode($doc["summary"]);
+                    }
+                }
+                elseif($_GET["dir"] === "down"){
+                    if(in_array($user['_id'], $doc["summary"][$_GET["sentence"]]["down"])){
+                        echo "false";
+                    }
+                    else {
+                        if (($pos = array_search($user['_id'], $doc["summary"][$_GET["sentence"]]["up"])) !== false) {
+                            unset($doc["summary"][$_GET["sentence"]]["up"][$pos]);
+                        }
+                        $doc["summary"][$_GET["sentence"]]["down"][] = $user["_id"];
+                        Domains::setDocument($_GET["domain"], $_GET["doc"], $doc);
+                        echo json_encode($doc["summary"]);
+                    }
+                }
+                else{
+                    echo "false";
+                }
+            }
+            else{
+                echo "false";
+            }
+        }
+    }
+    public function hasPermission(){
+        return SessionStore::hasSession();
+    }
+}
