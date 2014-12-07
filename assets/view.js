8d575c56115c42af671881a3da011c17e4c59487
Falkirks <bpnoreply@gmail.com>
2014-12-07 10:26:06 -0800

    Added button generation

--- assets/view.js
+++ assets/view.js
@@ -8,8 +8,24 @@ $(document).ready(function(){
             $(this).html('<div class="progress-bar progress-bar-success" style="width:'+ (up/(total))*100 + '%">'+ up + '</div><div class="progress-bar progress-bar-danger" style="width:' + (down/(total))*100 + '%">' + down + '</div>');
         });
     };
-    $('.rating-buttons').on("click", function(){
+    var generateButtons = function(){
+        $('.rating-holder .rating-buttons').each(function(index){
+            $(this).html('<a class="btn ' + (inArray(username, votes[index]["down"]) ? 'btn-default' : 'btn-danger') + ' downvote" sid="' + index + '">Downvote</a><a class="btn ' + (inArray(username, votes[index]["up"]) ? 'btn-default' : 'btn-success') + ' upvote" sid="' + index + '">Upvote</a>');
+        });
+    };
+    var inArray = function(needle, haystack) {
+        var length = haystack.length;
+        for(var i = 0; i < length; i++) {
+            if(haystack[i] == needle) return true;
+        }
+        return false;
+    };
+    $('.upvote').on("click", function(){
+        alert($(this).html());
+    });
+    $('.downvote').on("click", function(){
         alert($(this).html());
     });
     generateRatingBoxes();
+    generateButtons();
 });
