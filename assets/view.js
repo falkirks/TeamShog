afb91bf1820dea98ae3440a6dc3f7c9ed23e5077
Falkirks <bpnoreply@gmail.com>
2014-12-07 12:01:40 -0800

    Allow unregistered users to view votes

--- assets/view.js
+++ assets/view.js
@@ -1,53 +1,53 @@
 $(document).ready(function(){
     WordCloud(document.getElementById('wordCloud'), { list: cloudData, gridSize: 16, weightFactor: 10} );
-    if(username !== false) {
-        var generateRatingBoxes = function () {
-            $('.rating-holder .rating-bars').each(function (index) {
-                var up = votes[index][0].length;
-                var down = votes[index][1].length;
-                var total = up + down;
-                $(this).html('<div class="progress-bar progress-bar-success" style="width:' + (up / (total)) * 100 + '%">' + up + '</div><div class="progress-bar progress-bar-danger" style="width:' + (down / (total)) * 100 + '%">' + down + '</div>');
-            });
-        };
-        var generateButtons = function () {
-            $('.rating-holder .rating-buttons').each(function (index) {
-                $(this).html('<a class="btn ' + (inArray(username, votes[index][1]) ? 'btn-default' : 'btn-danger') + ' downvote" sid="' + index + '">Downvote</a><a class="btn ' + (inArray(username, votes[index][0]) ? 'btn-default' : 'btn-success') + ' upvote" sid="' + index + '">Upvote</a>');
-            });
-        };
-        var inArray = function (needle, haystack) {
-            var length = haystack.length;
-            for (var i = 0; i < length; i++) {
-                if (haystack[i] == needle) return true;
-            }
-            return false;
-        };
-        var fetchVotes = function(){
-            $.ajax({
-                url: "/vote/?domain=" + domain + "&doc=" + doc,
-                success: function(data){
-                    if (data != "false") {
-                        votes = JSON.parse(data);
-                        generateRatingBoxes();
-                        generateButtons()
+    var generateRatingBoxes = function () {
+        $('.rating-holder .rating-bars').each(function (index) {
+            var up = votes[index][0].length;
+            var down = votes[index][1].length;
+            var total = up + down;
+            $(this).html('<div class="progress-bar progress-bar-success" style="width:' + (up / (total)) * 100 + '%">' + up + '</div><div class="progress-bar progress-bar-danger" style="width:' + (down / (total)) * 100 + '%">' + down + '</div>');
+        });
+    };
+    var generateButtons = function () {
+        $('.rating-holder .rating-buttons').each(function (index) {
+            $(this).html('<a class="btn ' + (inArray(username, votes[index][1]) ? 'btn-default' : 'btn-danger') + ' downvote" sid="' + index + '">Downvote</a><a class="btn ' + (inArray(username, votes[index][0]) ? 'btn-default' : 'btn-success') + ' upvote" sid="' + index + '">Upvote</a>');
+        });
+    };
+    var inArray = function (needle, haystack) {
+        var length = haystack.length;
+        for (var i = 0; i < length; i++) {
+            if (haystack[i] == needle) return true;
+        }
+        return false;
+    };
+    var fetchVotes = function(){
+        $.ajax({
+            url: "/vote/?domain=" + domain + "&doc=" + doc,
+            success: function(data){
+                if (data != "false") {
+                    votes = JSON.parse(data);
+                    generateRatingBoxes();
+                    generateButtons()
 
-                    }
-                    else{
-                        alert("Error fetching votes.");
-                    }
                 }
-            });
-        };
+                else{
+                    alert("Error fetching votes.");
+                }
+            }
+        });
+    };
+    if(username != false) {
         $(document).on("click", '.downvote', function () {
             $.ajax({
                 url: "/vote/?dir=down&domain=" + domain + "&doc=" + doc + "&sentence=" + $(this).attr('sid'),
-                success: function(data){
+                success: function (data) {
                     if (data != "false") {
                         votes = JSON.parse(data);
                         generateRatingBoxes();
                         generateButtons()
 
                     }
-                    else{
+                    else {
                         alert("Vote error.");
                     }
                 }
@@ -56,19 +56,19 @@ $(document).ready(function(){
         $(document).on("click", '.upvote', function () {
             $.ajax({
                 url: "/vote/?dir=up&domain=" + domain + "&doc=" + doc + "&sentence=" + $(this).attr('sid'),
-                success: function(data){
+                success: function (data) {
                     if (data != "false") {
                         votes = JSON.parse(data);
                         generateRatingBoxes();
                         generateButtons()
 
                     }
-                    else{
+                    else {
                         alert("Vote error.");
                     }
                 }
             });
         });
-        fetchVotes();
     }
+    fetchVotes();
 });
