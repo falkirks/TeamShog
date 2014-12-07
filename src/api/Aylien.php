f4e03cde0f4c36052ff7d613ffe4ec620822f842
Falkirks <bpnoreply@gmail.com>
2014-12-06 21:04:09 -0800

    Possible fix

--- src/api/Aylien.php
+++ src/api/Aylien.php
@@ -7,6 +7,7 @@ class Aylien {
     public static function call_api($endpoint, $parameters) {
         $ch = curl_init('https://api.aylien.com/api/v1/' . $endpoint);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             'Accept: application/json',
             'X-AYLIEN-TextAPI-Application-Key: ' . 'ad04ec13da667f7236466e8f4c4d2a71',
